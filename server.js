3b670b2ebab2dde230436ef1ef9fe0fff4d62f71
Shaheed mon <sheheedk07@gmail.com>
2025-03-14 19:28:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT=process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/',(req,res)=>{
+res.send('api working')
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
