443986495ccd053cf813358b178100afb1335126
Shaheed mon <sheheedk07@gmail.com>
2025-03-22 00:17:31 +0530

    ongoing

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
 
 const PORT=process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+await connectDB()
 
 app.get('/',(req,res)=>{
 res.send('api working')
 })
 
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port http://localhost:${PORT}`)
 })
