fa08df5705f732809b86e1d5c6a5fdaf46f01b81
Shaheed mon <sheheedk07@gmail.com>
2025-03-25 01:16:40 +0530

    ongoing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,14 @@
 import userModel from "../models/userModel.js";
+import bycrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
+const registerUser=async(req,res)=>{
+    try{
+        const{name,email,password}=req.body;
+        if(!name||!email||!password){
+            return res.json({success:false,message:"Oops! It looks like some details are missing."})
+        }
+    }catch(err){
 
-
+    }
+}
