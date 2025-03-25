189406057229a49ff45c8111d2932d5c25979695
Shaheed mon <sheheedk07@gmail.com>
2025-03-26 00:25:55 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,27 @@
 import userModel from "../models/userModel.js";
-import bycrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bycrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const registerUser=async(req,res)=>{
-    try{
-        const{name,email,password}=req.body;
-        if(!name||!email||!password){
-            return res.json({success:false,message:"Oops! It looks like some details are missing."})
-        }
-    }catch(err){
-
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({
+        success: false,
+        message: "Oops! It looks like some details are missing.",
+      });
     }
-    
-}
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password, salt);
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    const newUser=new userModel(userData)
+    const user=await newUser.save()
+
+const token =jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+  } catch (err) {}
+};
