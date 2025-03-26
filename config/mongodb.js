1e4844a3381162f2fef551de3863df3b0cc7c9d2
Shaheed mon <sheheedk07@gmail.com>
2025-03-26 20:09:46 +0530

    commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
-
-const connectDB=async()=>{
-    mongoose.connection.on('connected',()=>{
-        console.log('DataBase Connected')        
-    })
-    await mongoose.connect(`${process.env.MONGODB_URI}/Textify`)
-}
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("DataBase Connected");
+    });
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "TexturAi",
+    });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 export default connectDB;
--- controllers/userController.js
+++ controllers/userController.js
@@ -18,43 +18,47 @@ const registerUser = async (req, res) => {
       email,
       password: hashedPassword,
     };
-    const newUser=new userModel(userData)
-    const user=await newUser.save()
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
 
-const token =jwt.sign({id:user._id},process.env.JWT_SECRET)
-res.json({success:true,token,user:{name:user.name}})
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, token, user: { name: user.name } });
   } catch (error) {
-    console.log(error)
-    res.json({success:false,message:error.message})
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User does not exist.",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return res.json({
+      success: true,
+      token,
+      user: { name: user.name },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
   }
 };
 
-const loginUser=async(req,res)=>{
-    try {
-        const {email,password}=req.body;
-        const user=await userModel.findOne({email})
-        if(!user){
-            return res.json({
-                success: false,
-                message: "User does not exist.",
-              });
-        }
-        const isMatch=await bcrypt.compare(password,user.password)
-
-        if(isMatch){
-            const token =jwt.sign({id:user._id},process.env.JWT_SECRET)
-res.json({success:true,token,user:{name:user.name}})
-            res.json({success:true,message:''})
-        }else{
-            return res.json({
-                success: false,
-                message: "Invalid credentials",
-              });
-        }
-    } catch (error) {
-        console.log(error)
-        res.json({success:false,message:error.message})
-    }
-}
-
-export {registerUser,loginUser}
+export { registerUser, loginUser };
