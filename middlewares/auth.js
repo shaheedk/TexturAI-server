423aa40ad0bfe70442008ae1ad231690c16ca917
Shaheed mon <sheheedk07@gmail.com>
2025-03-27 01:02:18 +0530

    commit

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const userAuth = async (req, res, next) => {
   const { token } = req.headers;
 
   if (!token) {
-    return res.json({ success: false, message: "Not Authorized Login Again" });
+    return res.json({ success: false, message: "Not Authorized Login Again" })
     try {
         const tokenDecode=jwt.verify(token,process.env.JWT_SECRET)
         if(tokenDecode.id){
@@ -12,8 +12,12 @@ const userAuth = async (req, res, next) => {
         }else{
             return res.json({success:false,message:'Not Authorized Login Again'})
         }
+        next()
     } catch (error) {
-        
+        res.json({success:false,message:error.message})
     }
   }
+    
 };
+
+export default userAuth;
